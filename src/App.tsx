503fe31a33e22d596ff90e4c64ea3393cb009627
hameedulhaq <hameed.ulhaq@auto.mynaghi.com>
2026-02-17 12:21:20 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,13 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-  <BrowserRouter basename="/csd-website/">
-  <App />
-</BrowserRouter>
+      <BrowserRouter  basename="/csd-website/">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
